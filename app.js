18d361c0568b53940d1e97ba9094737c10c040f4
piyanggoon <31122178+piyanggoon@users.noreply.github.com>
2020-09-20 23:37:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -34,203 +34,203 @@ const BNBAddress = '0xc2eed0f5a0dc28cfa895084bc0a9b8b8279ae492';
 const BLPContract = '0xc2eed0f5a0dc28cfa895084bc0a9b8b8279ae492';
 
 async function getPoolInfo(pair, address) {
-	let poolInfoMap = contract.methods.poolInfoMap(pair);
-	let poolUserInfoMap = contract.methods.poolUserInfoMap(pair, address);
-
-	return new Promise((resolve, reject) => {
-		poolInfoMap.call((err, res) => {
-			if (err)
-			  return reject(new Error('getPoolInfo'));
-
-			let accBakePerShare = res[2];
-			let accBakePerShareMultiple = 1e12;
-			poolUserInfoMap.call((err, res) => {
-				if (err)
-				  return reject(new Error('getPoolInfo'));
-
-				let amount = res[0];
-				let rewardDebt = res[1];
-				let pendingReward = (amount * accBakePerShare) / accBakePerShareMultiple - rewardDebt;
-
-				resolve({
-					liquidity: utils.token(amount),
-					pendingReward: utils.token(pendingReward)
-				});
-			});
-		});
-	});
+  let poolInfoMap = contract.methods.poolInfoMap(pair);
+  let poolUserInfoMap = contract.methods.poolUserInfoMap(pair, address);
+  
+  return new Promise((resolve, reject) => {
+    poolInfoMap.call((err, res) => {
+      if(err) 
+        return reject(new Error('getPoolInfo'));
+      
+      let accBakePerShare = res[2];
+      let accBakePerShareMultiple = 1E12;
+      poolUserInfoMap.call((err, res) => {
+        if(err)
+          return reject(new Error('getPoolInfo'));
+        
+        let amount = res[0];
+        let rewardDebt = res[1];
+        let pendingReward = (amount * accBakePerShare) / accBakePerShareMultiple - rewardDebt;
+        
+        resolve({
+          liquidity: utils.token(amount),
+          pendingReward: utils.token(pendingReward)
+        });
+      });
+    });
+  });
 }
 
 async function getBalance(contract, address) {
-	let url = 'https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=' + contract + '&address=' + address + '&tag=latest';
-
-	return new Promise((resolve, reject) => {
-		request.get(url, (err, res, body) => {
-			if (err || res.statusCode != 200)
-			  return reject(new Error('getBalance'));
-
-			let json = JSON.parse(body);
-			let balance = json.result;
-			    balance = utils.token(balance);
-
-			resolve(balance);
-		});
-	});
+  let url = 'https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=' + contract + '&address=' + address + '&tag=latest';
+  
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, res, body) => {
+      if(err || res.statusCode != 200)
+        return reject(new Error('getBalance'));
+      
+      let json = JSON.parse(body);
+      let balance = json.result;
+          balance = utils.token(balance);
+      
+      resolve(balance);
+    });
+  });
 }
 
 async function getSupply(contract) {
-	let url = 'https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=' + contract;
-
-	return new Promise((resolve, reject) => {
-		request.get(url, (err, res, body) => {
-			if (err || res.statusCode != 200)
-			  return reject(new Error('getSupply'));
-
-			let json = JSON.parse(body);
-			let supply = json.result;
-			    supply = utils.token(supply);
-
-			resolve(supply);
-		});
-	});
+  let url = 'https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=' + contract;
+  
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, res, body) => {
+      if(err || res.statusCode != 200)
+        return reject(new Error('getSupply'));
+      
+      let json = JSON.parse(body);
+      let supply = json.result;
+          supply = utils.token(supply);
+      
+      resolve(supply);
+    });
+  });
 }
 
 async function getBNBPrice() {
-	let url = 'https://api.binance.com/api/v1/depth?symbol=BNBUSDT&limit=5';
-
-	return new Promise((resolve, reject) => {
-		request.get(url, (err, res, body) => {
-			if (err || res.statusCode != 200)
-			  return reject(new Error('getBNBPrice'));
-
-			let json = JSON.parse(body);
-			let bid = parseFloat(json.bids[0][0]);
-			let ask = parseFloat(json.asks[0][0]);
-
-			resolve((bid + ask) / 2);
-		});
-	});
+  let url = 'https://api.binance.com/api/v1/depth?symbol=BNBUSDT&limit=5';
+  
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, res, body) => {
+      if(err || res.statusCode != 200) 
+        return reject(new Error('getBNBPrice'));
+      
+      let json = JSON.parse(body);
+      let bid = parseFloat(json.bids[0][0]);
+      let ask = parseFloat(json.asks[0][0]);
+      
+      resolve((bid + ask) / 2);
+    });
+  });
 }
 
 async function startHere() {
-	let bnbBalance = await getBalance(BNBContract, BNBAddress);
-	let bakeBalance = await getBalance(BAKEContract, BAKEAddress);
-	let blpSupply = await getSupply(BLPContract);
-
-	let bnbPool = await getPoolInfo(BNB_BAKE, myAddress);
-	let bakePool = await getPoolInfo(BAKE_BAKE, myAddress);
-
-	let bnb_bake = bakeBalance / bnbBalance;
-	    bnb_bake = bnb_bake - bnb_bake * 0.003;
-	let bnb_usd = await getBNBPrice();
-	let bake_usd = bnb_usd / (bakeBalance / bnbBalance);
-	let bnbValue = bnbBalance * bnb_usd;
-	let bakeValue = bakeBalance * bake_usd;
-
-	let bakePoolBal = await getBalance(BAKEContract, BAKEPoolAddress);
-	let bakePoolVal = bakePoolBal * bake_usd;
-
-	// BNB/BAKE
-	let myBLP = bnbPool.liquidity;
-	let poolShare = (myBLP / blpSupply) * 100;
-	let myBNB = (bnbBalance * poolShare) / 100;
-	let myBNBValue = myBNB * bnb_usd;
-	let myBAKE = (bakeBalance * poolShare) / 100;
-	let myBAKEValue = myBAKE * bake_usd;
-	let myBAKEMining = bnbPool.pendingReward;
-	let myBAKEMiningValue = myBAKEMining * bake_usd;
-	let poolValue = myBNBValue + myBAKEValue + myBAKEMiningValue;
-
-	// BAKE/BAKE
-	let myBAKEStake = bakePool.liquidity;
-	let myBAKEStakeValue = myBAKEStake * bake_usd;
-	let myBAKEStakeMining = bakePool.pendingReward;
-	let myBAKEStakeMiningValue = myBAKEStakeMining * bake_usd;
-	let BAKEPoolShare = myBAKEStake / bakePoolBal;
-	let BAKEPoolValue = myBAKEStakeValue + myBAKEStakeMiningValue;
-
-	let myBAKEWallet = await getBalance(BAKEContract, myAddress);
-	let myBAKEWalletValue = myBAKEWallet * bake_usd;
-	let totalBAKE = myBAKE + myBAKEMining + myBAKEStake + myBAKEStakeMining + myBAKEWalletValue;
-	let totalBAKEValue = totalBAKE * bake_usd;
-	let totalValue = myBNBValue + myBAKEValue + myBAKEMiningValue + (myBAKEStakeValue + myBAKEStakeMiningValue) + myBAKEWalletValue;
-
-	// now only calculate only in BNB/BAKE
-	let totalLossBNB = (BNBInvestment - myBNB) * 2;
-	let lossPercent = (totalLossBNB / 2 / BNBInvestment) * 100;
-
-	bnb_bake = utils.currency(bnb_bake, false);
-	bnb_usd = utils.currency(bnb_usd);
-	bake_usd = utils.currency(bake_usd, true, 4);
-	bnbBalance = utils.currency(bnbBalance, false, 8);
-	bnbValue = utils.currency(bnbValue);
-	bakeBalance = utils.currency(bakeBalance, false, 8);
-	bakeValue = utils.currency(bakeValue);
-
-	bakePoolBal = utils.currency(bakePoolBal, false, 8);
-	bakePoolVal = utils.currency(bakePoolVal);
-
-	// BNB/BAKE
-	myBNB = utils.currency(myBNB, false, 8);
-	myBNBValue = utils.currency(myBNBValue);
-	myBAKE = utils.currency(myBAKE, false, 8);
-	myBAKEValue = utils.currency(myBAKEValue);
-	myBAKEMining = utils.currency(myBAKEMining, false, 8);
-	myBAKEMiningValue = utils.currency(myBAKEMiningValue);
-	poolValue = utils.currency(poolValue);
-	poolShare = utils.currency(poolShare, false, 8);
-
-	// BAKE/BAKE
-	myBAKEStake = utils.currency(myBAKEStake, false, 8);
-	myBAKEStakeValue = utils.currency(myBAKEStakeValue);
-	myBAKEStakeMining = utils.currency(myBAKEStakeMining, false, 8);
-	myBAKEStakeMiningValue = utils.currency(myBAKEStakeMiningValue);
-	BAKEPoolShare = utils.currency(BAKEPoolShare, false, 8);
-	BAKEPoolValue = utils.currency(BAKEPoolValue);
-
-	myBAKEWallet = utils.currency(myBAKEWallet, false, 8);
-	myBAKEWalletValue = utils.currency(myBAKEWalletValue);
-	totalBAKE = utils.currency(totalBAKE, false, 8);
-	totalBAKEValue = utils.currency(totalBAKEValue);
-	totalValue = utils.currency(totalValue);
-
-	totalLossBNB = utils.currency(totalLossBNB, false, 8);
-	lossPercent = utils.currency(lossPercent, false, 2);
-
-	console.log('==============================');
-	console.log('1 BNB = ' + bnb_usd);
-	console.log('1 BNB = ' + bnb_bake + ' BAKE');
-	console.log('1 BAKE = ' + bake_usd);
-	console.log('==============================');
-	console.log('[BNB/BAKE]');
-	console.log('BNB (Pool) = ' + bnbBalance + ' (' + bnbValue + ')');
-	console.log('BAKE (Pool) = ' + bakeBalance + ' (' + bakeValue + ')');
-	console.log('==============================');
-	console.log('[BAKE/BAKE]');
-	console.log('BAKE (Pool) = ' + bakePoolBal + ' (' + bakePoolVal + ')');
-	console.log('==============================');
-	console.log('[BNB/BAKE]');
-	console.log('Pool Share = ' + poolShare + '%');
-	console.log('BNB = ' + myBNB + ' (' + myBNBValue + ')');
-	console.log('BAKE = ' + myBAKE + ' (' + myBAKEValue + ')');
-	console.log('BAKE (Mining) = ' + myBAKEMining + ' (' + myBAKEMiningValue + ')');
-	console.log('Pool Value = ' + poolValue);
-	console.log('==============================');
-	console.log('[BAKE/BAKE]');
-	console.log('Pool Share = ' + BAKEPoolShare + '%');
-	console.log('BAKE (Stake) = ' + myBAKEStake + ' (' + myBAKEStakeValue + ')');
-	console.log('BAKE (Mining) = ' + myBAKEStakeMining + ' (' + myBAKEStakeMiningValue + ')');
-	console.log('Pool Value = ' + BAKEPoolValue);
-	console.log('==============================');
-	console.log('BAKE (Wallet) = ' + myBAKEWallet + ' (' + myBAKEWalletValue + ')');
-	console.log('Total BNB = ' + myBNB + ' (' + myBNBValue + ')');
-	console.log('Total BAKE = ' + totalBAKE + ' (' + totalBAKEValue + ')');
-	console.log('Total Value = ' + totalValue);
-	console.log('==============================');
-	if (BNBInvestment > 0) {
-		console.log('Total Loss = ' + totalLossBNB + ' BNB (' + lossPercent + '%)');
-		console.log('==============================');
-	}
+  let bnbBalance = await getBalance(BNBContract, BNBAddress);
+  let bakeBalance = await getBalance(BAKEContract, BAKEAddress);
+  let blpSupply = await getSupply(BLPContract);
+  
+  let bnbPool = await getPoolInfo(BNB_BAKE, myAddress);
+  let bakePool = await getPoolInfo(BAKE_BAKE, myAddress);
+  
+  let bnb_bake = bakeBalance / bnbBalance;
+      bnb_bake = bnb_bake - (bnb_bake * 0.003);
+  let bnb_usd = await getBNBPrice();
+  let bake_usd = bnb_usd / (bakeBalance / bnbBalance);
+  let bnbValue = bnbBalance * bnb_usd;
+  let bakeValue = bakeBalance * bake_usd;
+  
+  let bakePoolBal = await getBalance(BAKEContract, BAKEPoolAddress);
+  let bakePoolVal = bakePoolBal * bake_usd;
+  
+  // BNB/BAKE
+  let myBLP = bnbPool.liquidity;
+  let poolShare = myBLP / blpSupply * 100;
+  let myBNB = bnbBalance * poolShare / 100;
+  let myBNBValue = myBNB * bnb_usd;
+  let myBAKE = bakeBalance * poolShare / 100;
+  let myBAKEValue = myBAKE * bake_usd;
+  let myBAKEMining = bnbPool.pendingReward;
+  let myBAKEMiningValue = myBAKEMining * bake_usd;
+  let poolValue = myBNBValue + myBAKEValue + myBAKEMiningValue;
+  
+  // BAKE/BAKE
+  let myBAKEStake = bakePool.liquidity;
+  let myBAKEStakeValue = myBAKEStake * bake_usd;
+  let myBAKEStakeMining = bakePool.pendingReward;
+  let myBAKEStakeMiningValue = myBAKEStakeMining * bake_usd;
+  let BAKEPoolShare = myBAKEStake / bakePoolBal;
+  let BAKEPoolValue = myBAKEStakeValue + myBAKEStakeMiningValue;
+  
+  let myBAKEWallet = await getBalance(BAKEContract, myAddress);
+  let myBAKEWalletValue = myBAKEWallet * bake_usd;
+  let totalBAKE = (myBAKE + myBAKEMining + myBAKEStake + myBAKEStakeMining + myBAKEWallet);
+  let totalBAKEValue = totalBAKE * bake_usd;
+  let totalValue = (myBNBValue + myBAKEValue + myBAKEMiningValue) + (myBAKEStakeValue + myBAKEStakeMiningValue) + (myBAKEWalletValue);
+  
+  // now only calculate only in BNB/BAKE
+  let totalLossBNB = (BNBInvestment - myBNB) * 2;
+  let lossPercent = (totalLossBNB / 2) / BNBInvestment * 100;
+  
+  bnb_bake = utils.currency(bnb_bake, false);
+  bnb_usd = utils.currency(bnb_usd);
+  bake_usd = utils.currency(bake_usd, true, 4);
+  bnbBalance = utils.currency(bnbBalance, false, 8);
+  bnbValue = utils.currency(bnbValue);
+  bakeBalance = utils.currency(bakeBalance, false, 8);
+  bakeValue = utils.currency(bakeValue);
+  
+  bakePoolBal = utils.currency(bakePoolBal, false, 8);
+  bakePoolVal = utils.currency(bakePoolVal);
+  
+  // BNB/BAKE
+  myBNB = utils.currency(myBNB, false, 8);
+  myBNBValue = utils.currency(myBNBValue);
+  myBAKE = utils.currency(myBAKE, false, 8);
+  myBAKEValue = utils.currency(myBAKEValue);
+  myBAKEMining = utils.currency(myBAKEMining, false, 8);
+  myBAKEMiningValue = utils.currency(myBAKEMiningValue);
+  poolValue = utils.currency(poolValue);
+  poolShare = utils.currency(poolShare, false, 8);
+  
+  // BAKE/BAKE
+  myBAKEStake = utils.currency(myBAKEStake, false, 8);
+  myBAKEStakeValue = utils.currency(myBAKEStakeValue);
+  myBAKEStakeMining = utils.currency(myBAKEStakeMining, false, 8);
+  myBAKEStakeMiningValue = utils.currency(myBAKEStakeMiningValue);
+  BAKEPoolShare = utils.currency(BAKEPoolShare, false, 8)
+  BAKEPoolValue = utils.currency(BAKEPoolValue);
+  
+  myBAKEWallet = utils.currency(myBAKEWallet, false, 8);
+  myBAKEWalletValue = utils.currency(myBAKEWalletValue);
+  totalBAKE = utils.currency(totalBAKE, false, 8)
+  totalBAKEValue = utils.currency(totalBAKEValue);
+  totalValue = utils.currency(totalValue);
+  
+  totalLossBNB = utils.currency(totalLossBNB, false, 8)
+  lossPercent = utils.currency(lossPercent, false, 2)
+  
+  console.log('==============================')
+  console.log('1 BNB = ' + bnb_usd)
+  console.log('1 BNB = ' + bnb_bake + ' BAKE')
+  console.log('1 BAKE = ' + bake_usd)
+  console.log('==============================')
+  console.log('[BNB/BAKE]')
+  console.log('BNB (Pool) = ' + bnbBalance + ' (' + bnbValue + ')')
+  console.log('BAKE (Pool) = ' + bakeBalance + ' (' + bakeValue + ')')
+  console.log('==============================')
+  console.log('[BAKE/BAKE]')
+  console.log('BAKE (Pool) = ' + bakePoolBal + ' (' + bakePoolVal + ')')
+  console.log('==============================')
+  console.log('[BNB/BAKE]')
+  console.log('Pool Share = ' + poolShare + '%')
+  console.log('BNB = ' + myBNB + ' (' + myBNBValue + ')')
+  console.log('BAKE = ' + myBAKE + ' (' + myBAKEValue + ')')
+  console.log('BAKE (Mining) = ' + myBAKEMining + ' (' + myBAKEMiningValue + ')')
+  console.log('Pool Value = ' + poolValue)
+  console.log('==============================')
+  console.log('[BAKE/BAKE]')
+  console.log('Pool Share = ' + BAKEPoolShare  + '%')
+  console.log('BAKE (Stake) = ' + myBAKEStake + ' (' + myBAKEStakeValue +')')
+  console.log('BAKE (Mining) = ' + myBAKEStakeMining + ' (' + myBAKEStakeMiningValue + ')')
+  console.log('Pool Value = ' + BAKEPoolValue)
+  console.log('==============================')
+  console.log('BAKE (Wallet) = ' + myBAKEWallet + ' (' + myBAKEWalletValue + ')')
+  console.log('Total BNB = ' + myBNB + ' (' + myBNBValue + ')')
+  console.log('Total BAKE = ' + totalBAKE + ' (' + totalBAKEValue + ')')
+  console.log('Total Value = ' + totalValue)
+  console.log('==============================')
+  if(BNBInvestment > 0) {
+    console.log('Total Loss = ' + totalLossBNB + ' BNB (' + lossPercent + '%)')
+    console.log('==============================')
+  }
 }
 
 startHere();
